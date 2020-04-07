2ec82601caee33f1f26de8747e1e3e5436778331
Juan Gallo <juanfgallo@experimentality.co>
2020-04-06 19:28:27 -0500

    determon

--- controller/thomson-creation/construction/ToDeterministic.js
+++ controller/thomson-creation/construction/ToDeterministic.js
@@ -1,7 +1,48 @@
 class ToDeterministic {
-    constructor() { }
-    
-    convertToDeterministic() {
-        
+  constructor() {
+    this.visited = [];
+    this.index = 0;
+    this.arrayLamda = [];
+  }
+
+  convertToDeterministic() {}
+  setIdentifiersLambda(node, Identifier) {
+    if (node.returnIdentifier() == 0) {
+      node.assignIdentifier(Identifier);
+      while (Identifier == this.index) {
+        console.log(this.index);
+        console.log(this.arrayLamda);
+        this.visited.push(node);
+        console.log(this.visited);
+        if (
+          node.returnLeft() !== null &&
+          node.returnLeft().returnData() === "λ"
+        ) {
+          Identifier++;
+          this.arrayLamda.push[this.index++];
+          console.log(this.arrayLamda);
+          Identifier = this.setIdentifiersLambda(node.returnLeft(), Identifier);
+        }
+        if (
+          node.returnRight() !== null &&
+          node.returnRight().returnData() === "λ"
+        ) {
+          Identifier++;
+          this.arrayLamda.push[this.index++];
+          console.log(this.arrayLamda);
+          Identifier = this.setIdentifiersLambda(
+            node.returnRight(),
+            Identifier
+          );
+        }
+      }
+      if (node.returnRight() === null) {
+        Identifier--;
+        this.index--;
+      }
     }
+    return Identifier;
+  }
 }
+
+module.exports = ToDeterministic;
